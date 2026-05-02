e90f67ae1b96c33c177753d4737f16ea22b0e17d
Sheilane Isidio S. Araujo <156730765+SheilaneIA11@users.noreply.github.com>
2026-05-02 22:41:22 +0000

    classe principal e responsavél por executar o programa no terminal/console

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
-public class Main {
+public class Main {    //classe principal e responsavél por executar o programa no terminal/console
     public static void main(String[] args) {
+
+              //instanciando um objeto da classe personagem com valores definidos
         Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
 
-        .
-        .
-        .
+		heroi1.exibirStatus();       //chamando o método para exibir os dados do personagem no console
 
     }
 }
