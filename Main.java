eba4572aaaa69c029e6b346bc83e1f5886e060f7
Sheilane Isidio S. Araujo <156730765+SheilaneIA11@users.noreply.github.com>
2026-05-02 22:43:42 +0000

    instanciando um objeto da classe personagem com valores definidos

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {    //classe principal e responsavél por executar o programa no terminal/console
     public static void main(String[] args) {
 
-              //instanciando um objeto da classe personagem com valores definidos
-        Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+         //instanciando um objeto da classe personagem com valores definidos
+        Personagem heroi1 = new Personagem("Arthemis", "Arqueira", 5, 60, 12.5);
 
 		heroi1.exibirStatus();       //chamando o método para exibir os dados do personagem no console
 
