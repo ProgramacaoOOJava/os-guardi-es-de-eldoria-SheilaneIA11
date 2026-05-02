c9ecbb77c70785309b54f373d0affdc76f8495ee
Sheilane Isidio S. Araujo <156730765+SheilaneIA11@users.noreply.github.com>
2026-05-02 22:32:10 +0000

    construtor responsável por iniciar o objeto com os dados recebidos

--- Personagem.java
+++ Personagem.java
@@ -6,8 +6,9 @@ public class Personagem {     //criando classe para personagem
     int nivel;
     int pontosDeVida;
     double poderBase;
-
-    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
+    
+    //construtor responsável por iniciar o objeto com os dados recebidos
+    public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {  
         this.nome = nome;
     }
 
